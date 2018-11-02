5cf423ab4c193bb37d2791f1db9996618c36a8fe
javalitterboy <javalitterboy@qq.com>
2018-11-02 18:07:11 +0800

    初始化我自己的需求

--- src/main/java/com/javalitterboy/api_tencent/controll/IPController.java
+++ src/main/java/com/javalitterboy/api_tencent/controll/IPController.java
@@ -1,10 +1,42 @@
 package com.javalitterboy.api_tencent.controll;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.javalitterboy.api_tencent.service.RecordService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
 /**
  * @program: api_tencent
  * @description:
  * @author: JavaLitterBoy
  * @create: 2018-11-02 17:54
  **/
+@RestController
 public class IPController {
+
+    @Resource
+    private RecordService recordService;
+
+    @GetMapping("/update_ip")
+    public String update_ip(@RequestParam("ip") String ip){
+        try {
+            String subDomains[] = {"*","www"};
+            for(String subDomain:subDomains) {
+                JSONArray records = recordService.record_list("stustyle.cn", subDomain, "A");
+                for (Object object : records) {
+                    recordService.update_record("stustyle.cn", (int) (((JSONObject) object).get("id")),
+                            subDomain, "A", "默认", ip, null, null);
+                }
+            }
+            return "请求成功";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "请求失败";
+        }
+    }
 }
