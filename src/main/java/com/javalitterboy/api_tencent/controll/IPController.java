251414f0f3cbc9ab70f8a1d5437cb98fcd6e335f
gongbo <javalitterboy@gmail.com>
2020-03-14 16:17:02 +0800

    修改配置文件

--- src/main/java/com/javalitterboy/api_tencent/controll/IPController.java
+++ src/main/java/com/javalitterboy/api_tencent/controll/IPController.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.javalitterboy.api_tencent.service.RecordService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +22,31 @@ import java.io.IOException;
 @RestController
 public class IPController {
 
+    @Value("api.domain")
+    private String domain;
+
+    @Value("api.sub_domains")
+    private String[] subDomains;
+
     @Resource
     private RecordService recordService;
 
     private static Logger logger = LoggerFactory.getLogger(IPController.class);
 
+    @GetMapping("/")
+    public String index(){
+        return "hello frp";
+    }
+
     @GetMapping("/update_ip")
     public String update_ip(@RequestParam("ip") String ip){
         try {
             logger.info("更换ip:"+ip);
-            String subDomains[] = {"*","www"};
+//            String subDomains[] = {"*","www"};
             for(String subDomain:subDomains) {
-                JSONArray records = recordService.record_list("stustyle.cn", subDomain, "A");
+                JSONArray records = recordService.record_list(domain, subDomain, "A");
                 for (Object object : records) {
-                    recordService.update_record("stustyle.cn", (int) (((JSONObject) object).get("id")),
+                    recordService.update_record(domain, (int) (((JSONObject) object).get("id")),
                             subDomain, "A", "默认", ip, null, null);
                 }
             }
